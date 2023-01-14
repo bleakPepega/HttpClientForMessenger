8a7f8415510c6a9be5fd60b40c795b5ad26b374e
bleak_Pepega <matraskin879@gmail.com>
2023-01-15 01:38:34 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    dependencies {
+        implementation("io.ktor:ktor-server-core:2.2.2")
+        implementation("io.ktor:ktor-server-netty:2.2.2")
+        implementation("io.ktor:ktor-client-core:2.2.2")
+        implementation("io.ktor:ktor-client-cio:2.2.2")
+        implementation("io.ktor:ktor-client-okhttp:2.2.2")
+        implementation("io.ktor:ktor-client-logging:2.2.2")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.30")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main() {
+    val messages = mutableListOf<ByteArray>()
+
+    embeddedServer(Netty, port = 4567) {
+        routing {
+            post("/messages") {
+                val message = call.receive<ByteArray>()
+                messages.add(message)
+                call.respondText("Message received!")
+            }
+            get("/messages") {
+                val lastMessage = messages.lastOrNull()
+                if (lastMessage != null) {
+                    call.respond(lastMessage)
+                } else {
+                    call.respondText("No messages received yet")
+                }
+            }
+        }
+    }.start(wait = true)
+}
+
+
+
